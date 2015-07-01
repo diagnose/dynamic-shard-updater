ccf2a08a20b62dfefefced74023231d4cb4b6651
Brandon Berney <brandonberney@protonmail.com>
2015-07-01 18:38:58 -0400

    Added something very light as a start.

---
+++ shard.py
@@ -0,0 +1,38 @@
+import ctypes
+import os
+import platform
+import textwrap
+import threading
+import time
+
+from shard import constants
+
+
+class ShardUpdater:
+    def __init__(self):
+        if platform.system() == constants.PLATFORM_WINDOWS:
+            ctypes.windll.kernel32.SetConsoleTitleA(constants.TITLE)
+
+        self.CheckSettings()
+
+    def CheckSettings(self):
+        if os.path.isfile(constants.SETTINGS_FILE):
+            # This user already has a settings file. We don't need to bug them.
+            # TODO: Verify everything we need is actually in the settings file!
+            self.RunScanner()
+        else:
+            print(constants.NO_SETTINGS)
+
+    def Invoker(self):
+        pass
+
+    def InvokerParser(self, command):
+        command = command.split(constants.COMMAND_SPACE)
+        if command[0] == constants.COMMAND_HELP:
+            print(constants.GENERAL_HELP)
+            print(constants.COMMANDS)
+
+    def RunScanner(self):
+        pass
+
+ShardUpdater = ShardUpdater()
---
+++ shard/constants.py
@@ -0,0 +1,19 @@
+# Filenames
+SETTINGS_FILE = 'settings.json'
+
+# Commands
+COMMAND_HELP = 'help'
+COMMAND_SPACE = ' '
+
+# Platforms
+PLATFORM_WINDOWS = 'Windows'
+
+# Prompts
+TITLE = b'Toontown Shard Updater'
+COMMANDS = '\nhelp - Prints this help documentation.'
+NO_SETTINGS = 'It appears this is your first time using this program. ' \
+              'We need to ask you some information before you can use ' \
+              'the district updater.'
+GENERAL_HELP = 'The Toontown Stream Shard Updater makes it easy to switch ' \
+               'districts while livestreaming, as your district name is ' \
+               'automatically updated!'
