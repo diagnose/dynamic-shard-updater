bb9d06b0720508b4e311220568e549c20cc04c47
Brandon Berney <brandonberney@protonmail.com>
2015-07-01 21:09:08 -0400

    shard: Raise an error if a command isn't valid.

--- shard/constants.py
+++ shard/constants.py
@@ -1,23 +1,35 @@
 # Filenames
 DISTRICT_FILE = 'district.txt'
 SETTINGS_FILE = 'settings.json'
+SHARDS_FILE = 'shards.json'
 
 # Commands
+COMMAND_CLEAR = 'clear'
+COMMAND_EXIT = 'exit'
 COMMAND_HELP = 'help'
 COMMAND_SPACE = ' '
 
 # Platforms
 PLATFORM_WINDOWS = 'Windows'
 
+# Logging
+CURRENT_DISTRICT = 'Current District: %s'
+DISTRICT_UNKNOWN = 'UNKNOWN'
+SHARD_TEXT = 'name'
+OTP_ENTERING_SHARD = ':OTPClientRepository: Entering shard'
+
 # Prompts
 TITLE = b'Toontown Shard Updater'
 COMMANDS = '\nhelp - Prints this help documentation.'
-INVALID_DIRECTORY = "The directory you specified doesn't exist."
-INVALID_INTERVAL = 'You have entered an invalid interval. It must be in ' \
+INVALID_COMMAND = '%s: command not found'
+INVALID_DIRECTORY = "The directory specified doesn't exist."
+INVALID_INTERVAL = 'An invalid interval was specified. It must be in ' \
                    'integer form (only numbers).'
+NEW_SHARD = 'Shard %d entered. Name %s detected.'
 NO_SETTINGS = 'It appears this is your first time using this program. ' \
               'We need to ask you some information before you can use ' \
               'the district updater.'
 GENERAL_HELP = 'The Toontown Stream Shard Updater makes it easy to switch ' \
                'districts while livestreaming, as your district name is ' \
                'automatically updated!'
+STARTING = 'Starting...'
